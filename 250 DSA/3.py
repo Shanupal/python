379a112eec453ccff3996db4a32b4a50cf24a286
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-23 00:02:24 +0530

    Add files via upload

---
+++ 250 DSA/3.py
@@ -0,0 +1,29 @@
+# class solution
+# def profir(self, price: list[int])
+# max = 0
+#      if prices ([l] <[r]):
+#          profir = prices ([r]-[l]):
+#              max = more(profir,prices)
+#     else
+#     l=r
+#     r +=1
+#     return max
+             
+             
+             
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        max_profit = 0 
+        l, r = 0, 1    
+
+        while r < len(prices):
+            if prices[l] < prices[r]:  
+                profit = prices[r] - prices[l]  
+                max_profit = max(max_profit, profit)  
+            else:
+                l = r  
+            r += 1  
+
+        return max_profit
+
+    
