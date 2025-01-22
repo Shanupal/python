d289d1483a4573ba1b80894025d92642971720f1
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-22 09:07:03 +0530

    Update 3.py

--- loop/3.py
+++ loop/3.py
@@ -6,10 +6,14 @@ class car:
      return f"{self.brabd}{self.model}"
  
  
-Class electricar(car):
+class electricar(car):
      def __init__(self, brand, model,battery_size):
          super().__init__(brand, model)
          self.battery_size = battery_size
-         
+     def fulltypr():
+         return "electric charge"
+             
+
 my_tesla = electricar("Tesla", "Model S", "85kWh")
-print(my_tesla)
+print(my_tesla.model)
+print(my_tesla.fuel_type())
