de21dd7bcc79c1d492b7d10743dfa2dc3ce9d422
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-22 21:47:19 +0530

    Add files via upload

---
+++ youtube manger app/youtube.py
@@ -0,0 +1,16 @@
+def list_all_videos():
+    
+
+
+while True:
+    print("\n youtube manger | option an app")
+    print("1 list all youtube video")
+    print("2 like all the video")
+    print("3 comment all the video")
+    print("4 subsrube my channel")
+    print("5 notofication my channel")
+    
+    choice = input("enter the number")
+    match choice :
+        case "1":
+            list_all_videos(video)   
