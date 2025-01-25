efb3b352f6b5babffa78d25361f3fee68e6c3c0c
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-25 19:17:35 +0530

    Update youtube.py

--- youtube manger app/youtube.py
+++ youtube manger app/youtube.py
@@ -2,16 +2,30 @@ import json
 
 def load_data():
     try:
-        with open('yorutube.txt','r')
-    except:
+        with open('yorutube.txt','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
         pass
+        return []
+
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(video,file)
+
 
 def list_all_videos():
-    pass
+    for index, video in enumerate(video, start=1):
+        print(f"{index}.{video["name"]},duration :{video["time"]}")
+        
+def add_video(video):
+    name = input("enter video name :")
+    time = input("enter the time :")
+    video.append({'name':name,'time':time})
+    save_data_helper(video)
+    
 def update_video(video):
     pass
-def update_video(video):
-    pass
+
 def delete_video(video):
     pass
 
@@ -22,8 +36,8 @@ while True:
     print("3 comment all the video")
     print("4 subsrube my channel")
     print("5 notofication my channel")
+    choice = input("enter your choice")
     
-    choice = input("enter the number")
     match choice :
         case "1":
             list_all_videos(video)   
@@ -36,5 +50,5 @@ while True:
         case "5":
             break
         case "6":
-        print("invalid choice")
+            print("invalid choice")
         
