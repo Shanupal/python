1531577f6318a436dd440aec6ab40573f5ac84bb
Shanupal <164392576+Shanupal@users.noreply.github.com>
2025-01-26 00:05:43 +0530

    Update youtube.py

--- youtube manger app/youtube.py
+++ youtube manger app/youtube.py
@@ -14,8 +14,12 @@ def save_data_helper(videos):
 
 
 def list_all_videos():
-    for index, video in enumerate(video, start=1):
+    print("\n")
+    print("*" * 70)
+    for index, video in enumerate(video, start=1):    
         print(f"{index}.{video["name"]},duration :{video["time"]}")
+        print("\n")
+        print("*" * 70)
         
 def add_video(video):
     name = input("enter video name :")
@@ -24,10 +28,24 @@ def add_video(video):
     save_data_helper(video)
     
 def update_video(video):
-    pass
+    list_all_videos(video)
+    index = int(input("enter the number to update"))
+    if 1<= index <=len(video):
+        name = input("enter the new video name")
+        time = input("enter the new time")
+        videos[index-1] ={'name' :name,'time': time}
+        save_data_helper(videos)
+    else:
+        print("invalid index selected")
 
 def delete_video(video):
-    pass
+    list_all_videos(videos)
+    index = int(input("enter the video number to be deleted"))
+    if 1<= index <= len(video):
+        del video[index-1]
+        save_data_helper(videos)
+    else:
+        print("invalid video index selected")
 
 while True:
     print("\n youtube manger | option an app")
@@ -51,4 +69,3 @@ while True:
             break
         case "6":
             print("invalid choice")
-        
